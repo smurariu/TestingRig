993a16cd74cce1f2a43df97a9d4207b9ff4c8c17
Serban Murariu <Serban.Murariu@sundiogroup.com>
2017-11-23 14:42:37 +0100

    Added sample app

---
+++ sample/SampleApp/SampleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SampleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var testResults = TestingRig.Do.ABTest(
+                a: () => { IEnumerable<int> e = new List<int> { 42 }; },
+                b: () => { IEnumerable<int> e = new[] { 42 }; });
+
+            Console.WriteLine("Elapsed microseconds: " + testResults.T1);
+            Console.WriteLine("Elapsed microseconds: " + testResults.T2);
+        }
+    }
+}
